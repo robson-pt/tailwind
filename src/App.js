28f7b58aee35e0d0103cf308a5249f74fda473e7
Robson <estudosti.ads.pt@gmail.com>
2023-03-21 21:30:15 +0000

    Add Icons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="container bg-gray-900 w-screen h-screen ">
+      <div className="wrapper">
+        <h1 className="text-4xl text-indigo-100 mb-4">Login</h1>
+        <input className="input" type="text" name="user" id="user" />
+        <input
+          className="input"
+          type="password"
+          name="password"
+          id="password"
+        />
+        <button className="button">Hello</button>
+      </div>
     </div>
   );
 }
